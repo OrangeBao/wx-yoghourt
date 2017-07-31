4088bcc5e2e761c47a78eae67e6246b185e7cfe3
baoyinghai <yhbao@amarsoft.com>
2017-07-31 15:52:51 +0800

    refactor: remove doc

--- doc/init.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO production (title, month_sell, price, img_id) VALUES ('葵花籽酸奶', 10, 15, 'title'), ('蔓越莓酸奶', 100, 17, 'title'), ('葡萄干酸奶', 10, 20, 'title'), ('原味酸奶', 8, 8, 'title');
-
-INSERT INTO address (address, name, sex, phone) VALUES ('江苏省苏州市高新区科技成', '鲍盈海', 0, '15250051111'), ('江苏省苏州市高新区东渚镇', '鲍瑾萱', 0, '15250051112');
-INSERT INTO u_a_relative (open_id, address_id) VALUES ('0001', 1), ('0001', 2);
-
